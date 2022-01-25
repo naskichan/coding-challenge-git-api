2bd3780871df3c1dc18bca6e6ff8c4ab60abd2b0
Naskichan <gitlab@naski.org>
2022-01-25 19:35:04 +0100

    using button component

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,20 @@
 import styled from '@emotion/styled'
+import Button from './Button'
 function User({props}) {
+    function handleClick() {
+        console.log('favorite')
+    }
     return (
-      <Wrapper>
-          <p>Username: {props.login}</p>
-          <p>ID: {props.id}</p>
-      </Wrapper>  
+    <>
+        <InformationWrapper>
+            <p>Username: {props.login}</p>
+            <p>ID: {props.id}</p>
+        </InformationWrapper>
+        <Button label="Hello World" onClick={handleClick}/>
+    </>
     )
 }
-const Wrapper = styled.div`
+const InformationWrapper = styled.div`
     margin: 1rem;
     -webkit-box-shadow: 10px 10px 15px 5px rgba(0,0,0,0.17); 
     box-shadow: 10px 10px 15px 5px rgba(0,0,0,0.17);
