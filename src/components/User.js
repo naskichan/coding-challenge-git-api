29e797eb0f436e98856d5c4d29d9c21b1625a449
Naskichan <gitlab@naski.org>
2022-01-25 19:37:32 +0100

    fixing css mislayouting

--- src/components/User.js
+++ src/components/User.js
@@ -5,26 +5,31 @@ function User({props}) {
         console.log('favorite')
     }
     return (
-    <>
+    <MainWrapper>
         <InformationWrapper>
             <p>Username: {props.login}</p>
             <p>ID: {props.id}</p>
         </InformationWrapper>
         <Button label="Hello World" onClick={handleClick}/>
-    </>
+    </MainWrapper>
     )
 }
-const InformationWrapper = styled.div`
+const MainWrapper = styled.div`
     margin: 1rem;
     -webkit-box-shadow: 10px 10px 15px 5px rgba(0,0,0,0.17); 
     box-shadow: 10px 10px 15px 5px rgba(0,0,0,0.17);
     padding: 1rem;
     border-radius: 1rem;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    justify-content: space-between;
     max-width: 50%;
     p{
         all: unset;
     }
+`
+const InformationWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
 `
 export default User
