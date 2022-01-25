549105689772bc2b34cb53e0bc6d933288903c21
Naskichan <gitlab@naski.org>
2022-01-25 22:47:55 +0100

    rebuilding button to use image

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,22 @@
+import styled from '@emotion/styled'
+import Image from './Image'
+
 function Button(props) {
     return (
-        <button onClick={props.onClick}>
-            {props.label}
-        </button>
+        <ButtonWrapper onClick={props.onClick}>
+            {props.favorize ? 
+            <ButtonImage src='/images/star-filled.png' alt='star' /> :
+            <ButtonImage src='/images/star.png' alt='star' />
+        }
+        </ButtonWrapper>
     )
 }
+
+const ButtonWrapper = styled.div`
+height: 2rem;
+`
+const ButtonImage = styled.img`
+height: 100%;
+`
 
 export default Button
