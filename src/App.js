9db798538ff737b0b7cf0b453584e84b79cf625a
Naskichan <gitlab@naski.org>
2022-01-25 18:54:26 +0100

    basic state implementation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-
-import './App.css';
+import { useState } from "react"
+import './App.css'
 import styled from '@emotion/styled'
 
 function App() {
+  let [searchText, setSearchText] = useState('');
   return (
     <Page>
       <p>Hello World</p>
+      <Input onChange={event => setSearchText(event.target.value)} />
+      {searchText}
     </Page>
   );
 }
 const Page = styled.div`
   display: flex;
-  flex-direction: row;
-  margin: 2rem;
+  flex-direction: column;
+  margin: 5rem;
+  font-size: 1.5rem;
+`
+const Input = styled.input`
+  width: 50%;
 `
 
 export default App;
