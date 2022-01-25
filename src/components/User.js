4d5e25ff2ce231589f1960aef9cb6c9d51ca1697
Naskichan <gitlab@naski.org>
2022-01-25 19:29:07 +0100

    small styling adjustments

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,23 @@
-
+import styled from '@emotion/styled'
 function User({props}) {
     return (
-      <div>
-          {props.login}
-          {props.id}
-      </div>  
+      <Wrapper>
+          <p>Username: {props.login}</p>
+          <p>ID: {props.id}</p>
+      </Wrapper>  
     )
 }
-
+const Wrapper = styled.div`
+    margin: 1rem;
+    -webkit-box-shadow: 10px 10px 15px 5px rgba(0,0,0,0.17); 
+    box-shadow: 10px 10px 15px 5px rgba(0,0,0,0.17);
+    padding: 1rem;
+    border-radius: 1rem;
+    display: flex;
+    flex-direction: column;
+    max-width: 50%;
+    p{
+        all: unset;
+    }
+`
 export default User
